0f35efab932de4f6412ad98e78055d97a6d3b6fe
jgallaway2011 <jgallaway2011@gmail.com>
2021-09-02 17:13:40 -0400

    Setup server to recieve post request

---
+++ testserver.js
@@ -0,0 +1,34 @@
+const express = require("express");
+var fs = require('fs');
+// Initialize Express
+let app = express();
+
+// Configure middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function(request, response) {
+  console.log('GET /')
+  var html = `
+    <html>
+        <body>
+            <form method="post" action="http://localhost:3000">Name: 
+                <input type="text" name="name" />
+                <input type="submit" value="Submit" />
+            </form>
+        </body>
+    </html>`
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end(html)
+})
+
+app.post('/', function(request, response) {
+  console.log('POST /')
+  console.dir(request.body)
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end('thanks')
+})
+
+const port = 3000;
+app.listen(port);
+console.log(`Listening at http://localhost:${port}`);
