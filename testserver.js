001506a1b254772b201c5aa9ce41dd96fbc01303
jgallaway2011 <jgallaway2011@gmail.com>
2021-09-03 09:33:16 -0400

    Commenting out code

--- testserver.js
+++ testserver.js
@@ -7,26 +7,26 @@ let app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(request, response) {
-  console.log('GET /')
-  var html = `
-    <html>
-        <body>
-            <form method="post" action="https://test-server-webhook.herokuapp.com">Name: 
-                <input type="text" name="name" />
-                <input type="submit" value="Submit" />
-            </form>
-        </body>
-    </html>`
-  response.writeHead(200, {'Content-Type': 'text/html'})
-  response.end(html)
-})
+// app.get('/', function(request, response) {
+//   console.log('GET /')
+//   var html = `
+//     <html>
+//         <body>
+//             <form method="post" action="https://test-server-webhook.herokuapp.com">Name: 
+//                 <input type="text" name="name" />
+//                 <input type="submit" value="Submit" />
+//             </form>
+//         </body>
+//     </html>`
+//   response.writeHead(200, {'Content-Type': 'text/html'})
+//   response.end(html)
+// })
 
 app.post('/', function(request, response) {
   console.log('POST /')
   console.dir(request.body)
-  response.writeHead(200, {'Content-Type': 'text/html'})
-  response.end('thanks')
+  // response.writeHead(200, {'Content-Type': 'text/html'})
+  // response.end('thanks')
 })
 
 const port = process.env.PORT || 3000;
